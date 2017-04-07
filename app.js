9882cf87c5c02ca38055afd28ff606f817d0ba92
Lucian Pacurar <lucian.pacurar@visma.com>
2017-04-07 17:04:34 +0300

    Update API endpoints.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 let express = require("express");
+let bodyParser = require('body-parser');
 let app = express();
-
 let employeesRepo = require('./employees.js');
+const uuidV1 = require('uuid/v1');
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
@@ -11,6 +14,36 @@ app.get('/api/employees', function (req, res) {
     res.json(employeesRepo.employees);
 });
 
+app.post("/api/employees", function (req, res) {
+    let employee = req.body;
+    employee.id = uuidV1();
+    employeesRepo.employees.push(employee);
+    res.json(employee);
+});
+
+app.put("/api/employees/:id", function (req, res) {
+    let employee = req.body;
+    let id = req.params.id;
+    let foundEmployee = employeesRepo.employees.find(e => e.id === id);
+    if (foundEmployee) {
+      Object.assign(foundEmployee, employee);
+      res.json(employee);
+    } else {
+      res.send(400);
+    }
+});
+
+app.delete("/api/employees/:id", function (req, res) {
+  let id = req.params.id;
+  let employeeIndex = employeesRepo.employees.findIndex(e => e.id === id);
+  if (employeeIndex >= 0) {
+    employeesRepo.employees.splice(employeeIndex, 1);
+    res.send(200);
+  } else {
+    res.send(400);
+  }
+});
+
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Employee management api is running');
+    console.log(`Employee management api is running on port ${process.env.PORT || 3000}`);
 });
