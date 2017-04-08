6b295e0bb4e7ceb12b27ebb069f93ce3393fa2b7
Lucian Pacurar <luci.pacurar@gmail.com>
2017-04-08 18:26:30 +0300

    Add user update endpoit

--- app.js
+++ app.js
@@ -8,12 +8,6 @@ const uuidV1 = require('uuid/v1');
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
